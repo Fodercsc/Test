12289c4c35419ce013968a2dc95b9d6d81aa1686
foder <cscing@126.com>
2015-11-22 15:07:48 +0800

    changed branch brh

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class Hello{
 		public void test3(){}
 		public static void main(String args[]){
 			System.out.println("hello world!");
+			System.out.println("Happy New Year!");
+			System.out.println("hello Myself!");
 			System.out.println("csc");
 		}
 }
