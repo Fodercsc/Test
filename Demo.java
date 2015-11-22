d0dc68e1ccb2f0002561240eb723d657ec6e89e4
foder <cscing@126.com>
2015-11-22 08:24:49 +0800

    Add Demo.java

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo{
+		public void test(){}
+		public vodi test3(){}
+		public static void main(String args[]){
+			System.out.println("csc");
+			System.out.println("www.csc.com");
+		}
+}
