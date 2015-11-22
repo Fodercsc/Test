cfa8e0a017ba8b0372ee5b3a32405cce236f1fdc
foder <cscing@126.com>
2015-11-22 15:54:43 +0800

    add static attribute

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 		public void test(){}
 		public void test3(){}
 		public static void main(String args[]){
+			private static final String temp = "hello";
 			System.out.println("hello world!");
 			System.out.println("Happy New Year!");
 			System.out.println("hello Myself!");
