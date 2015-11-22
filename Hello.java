532dc9a437cf1f38c80b379f5b0aa8c37c1b756a
foder <cscing@126.com>
2015-11-22 10:53:58 +0800

    have changed Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 		public void test(){}
 		public void test3(){}
 		public static void main(String args[]){
-			System.out.println("csc");
+			System.out.println("hello world!");
 			System.out.println("csc");
 		}
 }
