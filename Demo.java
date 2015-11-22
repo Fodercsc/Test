a91defa3d7e120b562634141b9e063e716bd1ce7
foder <cscing@126.com>
2015-11-22 10:32:39 +0800

    change two files

--- Demo.java
+++ Demo.java
@@ -1,8 +1,7 @@
 public class Demo{
 		public void test(){}
-		public vodi test3(){}
+		public void test3(){}
 		public static void main(String args[]){
 			System.out.println("csc");
-			System.out.println("www.csc.com");
 		}
 }
--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
 public class Hello{
 		public void test(){}
-		public vodi test3(){}
+		public void test3(){}
 		public static void main(String args[]){
-			System.out.println("csc");
 			System.out.println("csc");
 			System.out.println("csc");
 		}
