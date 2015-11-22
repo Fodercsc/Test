6e3a4943022baf9d3577836c282899e21c8d3002
foder <cscing@126.com>
2015-11-22 08:11:02 +0800

    Update Hello.java File .Add Two Lines

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
 		public void test(){}
-		public vodi test2(){}
+		public vodi test3(){}
 		public static void main(String args[]){
-			System.out.println("Hello world!");
+			System.out.println("csc");
+			System.out.println("www.csc.com");
 		}
 }
